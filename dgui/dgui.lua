b8aee5aa4577dd71993d2acd306a12a5b92891a5
Klayton Kowalski <klaytonkowalski@gmail.com>
2021-06-19 23:13:27 -0400

    Pulled new dgui version from work in Hyperfocus Freefall. Further testing and improvements will be done over there, then copied here. Once dgui is in a well-off state, I will make the example projects and documentation.

--- dgui/dgui.lua
+++ dgui/dgui.lua
@@ -32,90 +32,203 @@
 
 local dgui = {}
 
-local components = {}
+dgui.enabled = false
 
-local action_ids = {
-	button_left = hash("button_left")
+dgui.group = nil
+dgui.groups = {}
+
+dgui.input_bindings = {
+	next = {},
+	previous = {},
+	select = {}
 }
 
-dgui.component = {
-	button = 1
+dgui.display_mode = nil
+dgui.display_modes = {
+	color = { idle = vmath.vector4(), selected = vmath.vector4() }
 }
 
 ----------------------------------------------------------------------
 -- FUNCTIONS
 ----------------------------------------------------------------------
 
-function dgui.add_component(component, node_id, enabled, callback, group, data)
-	components[node_id] = {
-		component = component,
-		node_id = node_id,
-		enabled = enabled,
-		callback = callback,
-		group = group,
-		data = data,
-		state = {
-			just_pressed = false,
-			just_hovered = false,
-			just_released = false,
-			just_exited = false,
-			is_pressed = false,
-			is_hovered = false
-		}
-	}
+local function make_transparent(color)
+	return vmath.vector4(color.x, color.y, color.z, 0)
+end
+
+local function get_node_index(group, id)
+	if dgui.groups[group] then
+		for key, value in ipairs(dgui.groups[group].nodes) do
+			if value.id == id then
+				return key
+			end
+		end
+	end
+end
+
+local function get_next_selectable_index(group)
+	if dgui.groups[group] then
+		local index = dgui.groups[group].index + 1
+		while index ~= dgui.groups[group].index do
+			if index > #dgui.groups[group].nodes then
+				index = 1
+			end
+			if dgui.groups[group].nodes[index].selectable then
+				return index
+			end
+			index = index + 1
+		end
+	end
+end
+
+local function get_previous_selectable_index(group)
+	if dgui.groups[group] then
+		local index = dgui.groups[group].index - 1
+		while index ~= dgui.groups[group].index do
+			if index < 1 then
+				index = #dgui.groups[group].nodes
+			end
+			if dgui.groups[group].nodes[index].selectable then
+				return index
+			end
+			index = index - 1
+		end
+	end
+end
+
+function dgui.set_enabled(enabled)
+	dgui.enabled = enabled
+end
+
+function dgui.set_input_bindings(next, previous, select)
+	for _, value in ipairs(next) do
+		dgui.input_bindings.next[value] = true
+	end
+	for _, value in ipairs(previous) do
+		dgui.input_bindings.previous[value] = true
+	end
+	for _, value in ipairs(select) do
+		dgui.input_bindings.select[value] = true
+	end
+end
+
+function dgui.set_display_mode(display_mode, arguments)
+	if display_mode == dgui.display_modes.color then
+		dgui.display_mode = display_mode
+		dgui.display_modes.color.idle = arguments.idle
+		dgui.display_modes.color.selected = arguments.selected
+	end
+end
+
+function dgui.get_group()
+	return dgui.group
+end
+
+function dgui.add_group(group)
+	if not dgui.groups[group] then
+		dgui.groups[group] = { nodes = {}, index = 1 }
+	end
 end
 
 function dgui.remove_group(group)
-	for key, value in pairs(components) do
-		if value.group == group then
-			components[key] = nil
-		end
+	if dgui.groups[group] then
+		-- todo: handle if group == dgui.group
+		dgui.groups[group] = nil
 	end
 end
 
-function dgui.set_node_enabled(node_id, enabled)
-	components[node_id].enabled = enabled
-end
-
-function dgui.set_group_enabled(group, enabled)
-	for _, value in pairs(components) do
-		if value.group == group then
-			value.enabled = enabled
+function dgui.set_group(group, reset)
+	if dgui.group then
+		if reset then
+			dgui.groups[dgui.group].index = 1
 		end
-	end
-end
-
-function dgui.set_action_ids(button_left)
-	action_ids.button_left = button_left
-end
-
-function dgui.on_input(action_id, action)
-	for key, value in pairs(components) do
-		local success, result = pcall(gui.get_node, key)
-		if success then
-			if gui.pick_node(result, action.x, action.y) then
-				value.state.just_hovered = not value.state.is_hovered
-				value.state.is_hovered = true
-				if value.component == dgui.component.button then
-					if action_id == action_ids.button_left then
-						value.state.just_pressed = action.pressed
-						value.state.just_released = action.released
-						value.state.is_pressed = action.value == 1
-					end
-				end
-			else
-				value.state.just_exited = value.state.is_hovered
-				value.state.is_pressed = false
-				value.state.is_hovered = false
+		local volatile_nodes = {}
+		for key, value in ipairs(dgui.groups[dgui.group].nodes) do
+			if dgui.display_mode == dgui.display_modes.color then
+				gui.set_color(gui.get_node(value.id), make_transparent(dgui.display_mode.idle))
+			end
+			if value.volatile then
+				table.insert(volatile_nodes, value.id)
 			end
 		end
-		if value.enabled and value.callback then
-			value.callback(value.state, value.node_id, value.data)
+		for _, value in ipairs(volatile_nodes) do
+			dgui.remove_node(dgui.group, value)
 		end
-		value.state.just_pressed = false
-		value.state.just_hovered = false
-		value.state.just_released = false
-		value.state.just_exited = false
+	end
+	dgui.group = group
+	if dgui.group then
+		for key, value in ipairs(dgui.groups[dgui.group].nodes) do
+			if dgui.display_mode == dgui.display_modes.color then
+				gui.set_color(gui.get_node(value.id), key == dgui.groups[dgui.group].index and dgui.display_mode.selected or dgui.display_mode.idle)
+			end
+		end
+	end
+end
+
+function dgui.get_node_id()
+	return dgui.groups[dgui.group].nodes[dgui.groups[dgui.group].index].id
+end
+
+function dgui.add_node(group, id, selectable, volatile)
+	if dgui.groups[group] and not get_node_index(group, id) then
+		table.insert(dgui.groups[group].nodes, { id = id, selectable = selectable, volatile = volatile })
+		if dgui.group == group then
+			if dgui.display_mode == dgui.display_modes.color then
+				gui.set_color(gui.get_node(id), dgui.display_mode.idle)
+			end
+		end
+	end
+end
+
+function dgui.remove_node(group, id)
+	local index = get_node_index(group, id)
+	if dgui.groups[group] and index then
+		if dgui.group == group then
+			-- todo: correct group index and currently selected node if needed
+			if dgui.display_mode == dgui.display_modes.color then
+				gui.set_color(gui.get_node(id), make_transparent(dgui.display_mode.idle))
+			end
+		end
+		table.remove(dgui.groups[group].nodes, index)
+	end
+end
+
+function dgui.swap_nodes(group, id_1, id_2)
+	local index_1 = get_node_index(group, id_1)
+	local index_2 = get_node_index(group, id_2)
+	if dgui.groups[group] and index_1 and index_2 then
+		local swap = dgui.groups[group].nodes[index_1]
+		dgui.groups[group].nodes[index_1] = dgui.groups[group].nodes[index_2]
+		dgui.groups[group].nodes[index_2] = swap
+		if dgui.group == group then
+			dgui.set_group(dgui.group)
+		end
+	end
+end
+
+function dgui.on_input(action, action_id)
+	if dgui.enabled then
+		if action.pressed then
+			if dgui.input_bindings.next[action_id] then
+				if dgui.display_mode == dgui.display_modes.color then
+					gui.set_color(gui.get_node(dgui.groups[dgui.group].nodes[dgui.groups[dgui.group].index].id), dgui.display_mode.idle)
+					dgui.groups[dgui.group].index = get_next_selectable_index(dgui.group)
+					gui.set_color(gui.get_node(dgui.groups[dgui.group].nodes[dgui.groups[dgui.group].index].id), dgui.display_mode.selected)
+				end
+			elseif dgui.input_bindings.previous[action_id] then
+				if dgui.display_mode == dgui.display_modes.color then
+					gui.set_color(gui.get_node(dgui.groups[dgui.group].nodes[dgui.groups[dgui.group].index].id), dgui.display_mode.idle)
+					dgui.groups[dgui.group].index = get_previous_selectable_index(dgui.group)
+					gui.set_color(gui.get_node(dgui.groups[dgui.group].nodes[dgui.groups[dgui.group].index].id), dgui.display_mode.selected)
+				end
+			elseif dgui.input_bindings.select[action_id] then
+				if dgui.display_mode == dgui.display_modes.color then
+					-- todo
+				end
+			end
+		elseif action.released then
+			-- todo
+		end
 	end
 end
 
